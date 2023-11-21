46fb86c2f3f97bea5657dc75f89b6313012849fd
jzuluaga02 <jzuluaga@scu.edu>
2023-11-20 17:08:46 -0800

    set up finished

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
-AUTHOR_USER_NAME = "krishnaik06"
-SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_USER_NAME = "jzuluaga02"
+SRC_REPO = "heart-disease-predictor"
+AUTHOR_EMAIL = "jzuluaga@scu.edu"
 
 
 setuptools.setup(
